d7973c21cde2aed0bfea69a9800b65a304c98f97
dhuruvan14 <mechyogesh24@gmail.com>
2022-03-12 16:17:00 +0530

    code added

--- src/cls/Practice1.java
+++ src/cls/Practice1.java
@@ -5,14 +5,21 @@ public class Practice1 {
 		int a=10;
 		System.out.println(a);
 	}
+	public void age() {
+		int b=5;
+		System.out.println(b);
+
+	}
+
+	
+	
+		
 	public static void main(String[] args) {
 		System.out.println(10);
 		
 	}
 		
-	}
-		
-	
+}
 		
 		
 	
