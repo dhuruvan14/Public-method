d1165d274edabb23c79804ab717a5b4203547b77
dhuruvan14 <mechyogesh24@gmail.com>
2022-03-12 15:58:29 +0530

    upload

---
+++ src/cls/Practice1.java
@@ -0,0 +1,20 @@
+package cls;
+
+public class Practice1 {
+	public void name() {
+		int a=10;
+		System.out.println(a);
+	}
+	public static void main(String[] args) {
+		System.out.println(10);
+		
+	}
+		
+	}
+		
+	
+		
+		
+	
+
+
